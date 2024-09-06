3e7d839b2395a5b288d9bbcddd58b5c461ece58b
Candy <168691418+Amiddala@users.noreply.github.com>
2024-09-05 21:19:07 -0400

    Clase Inicio

---
+++ OneDrive/Documentos/NetBeansProjects/MetodosYTecnicasDeProgramacion22024/src/main/java/com/mycompany/metodosytecnicasdeprogramacion22024/Inicio.java
@@ -0,0 +1,20 @@
+package com.mycompany.metodosytecnicasdeprogramacion22024;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Camila
+ */
+public class Inicio{ 
+        private int contra;
+        private String cargo;
+        public Inicio(int contra, String cargo){
+            this.contra = contra;
+            this.cargo = cargo;
+        }
+    
+}
