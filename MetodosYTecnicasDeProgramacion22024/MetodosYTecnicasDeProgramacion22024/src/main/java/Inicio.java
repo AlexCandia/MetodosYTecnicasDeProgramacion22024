3f4e5bc716265155facb54738ffcc48ca85f9703
edwcand <167575932+AlexCandia@users.noreply.github.com>
2024-09-14 20:37:13 -0400

    Parametro de registro objeto creado en vez de varios parametros

--- MetodosYTecnicasDeProgramacion22024/MetodosYTecnicasDeProgramacion22024/src/main/java/Inicio.java
+++ MetodosYTecnicasDeProgramacion22024/MetodosYTecnicasDeProgramacion22024/src/main/java/Inicio.java
@@ -8,20 +8,19 @@
  * @author developerweb
  */
 public class Inicio {
-    private GeneradorArchivo fileUser;
-    public Inicio(GeneradorArchivo generadorArchivo){
+    private GenerarArchivosUsuarios fileUser;
+    public Inicio(GenerarArchivosUsuarios generadorArchivo){
         fileUser=generadorArchivo;
     }
-    public boolean registrarUsuario(String nombre,String contraseña,String codigoE){
-        if(fileUser.buscarUsuario(codigoE)!= null){
+    public boolean registrarUsuario(Usuario usuario){
+        if(fileUser.buscarUsuario(usuario.getCodigoE())!= null){
             System.out.println("El usuario ya existe.");
             return false;
-        }else if(contraseña.length()<6){
+        }else if(usuario.getContraseña().length()<6){
             System.out.println("Contraseña muy corta.");
             return false;
         }
-        Usuario usuarioNuevo = new Usuario(nombre,contraseña,codigoE);
-        return fileUser.registrar(usuarioNuevo);
+        return fileUser.registrar(usuario);
     }
     public boolean login(String codigoE, String contraseña) {
         Usuario user = fileUser.buscarUsuario(codigoE);
