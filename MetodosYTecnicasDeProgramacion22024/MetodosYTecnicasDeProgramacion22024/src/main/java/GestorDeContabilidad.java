0d2404e64ca336cba7215812328ae5c5e6c72905
edwcand <167575932+AlexCandia@users.noreply.github.com>
2024-09-21 21:25:49 -0400

    Base para la contabilidad

---
+++ MetodosYTecnicasDeProgramacion22024/MetodosYTecnicasDeProgramacion22024/src/main/java/GestorDeContabilidad.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author vale
+ */
+import java.util.ArrayList;
+public class GestorDeContabilidad {
+
+    private double ingresosBoba;
+    private ArrayList<EgresoFijo> egresosFijos;
+    private ArrayList<EgresoInsumo> egresosInsumos;
+    private ArrayList<EgresoOtro> egresosOtros;
+    
+    public GestorDeContabilidad(){
+        ingresosBoba = 0;
+        egresosFijos = new ArrayList<>();
+        egresosInsumos = new ArrayList<>();
+        egresosOtros = new ArrayList<>();
+    }
+    
+    public String regristrarEgresoInsumo(){
+        return "";
+    }
+    
+    public String registrarEgresoOtro(){
+        return "";
+    }
+    
+    public String editarEgresoFijo(){
+        return "";
+    }
+    
+    public void imprimirReporteMensual(){
+    
+    }
+    
+    public void imprimirReporteHistorico(){
+    
+    }
+    
+    private boolean actualizarInventario(){
+      return false; 
+    }
+}
