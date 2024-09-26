3fadafaef43e07b5db813a58919b57a082b3abed
Candy <168691418+Amiddala@users.noreply.github.com>
2024-09-25 20:42:55 -0400

    Create EgresoVariableInsumo.java

---
+++ MetodosYTecnicasDeProgramacion22024/MetodosYTecnicasDeProgramacion22024/src/main/java/EgresoVariableInsumo.java
@@ -0,0 +1,23 @@
+public class EgresoVariableInsumo {
+    private String nombre;
+    private int cantidad;
+    private double valor;
+
+    public EgresoVariableInsumo(String nombre, int cantidad, double valor) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.valor = valor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
