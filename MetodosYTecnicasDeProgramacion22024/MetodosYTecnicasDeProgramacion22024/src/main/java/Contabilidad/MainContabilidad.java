f3af214eefe24fc3c2b9d6452ea46d62cf714d59
edwcand <167575932+AlexCandia@users.noreply.github.com>
2024-09-28 13:22:44 -0400

    Paquete Contabilidad

---
+++ MetodosYTecnicasDeProgramacion22024/MetodosYTecnicasDeProgramacion22024/src/main/java/Contabilidad/MainContabilidad.java
@@ -0,0 +1,42 @@
+package Contabilidad;
+
+
+import Contabilidad.GestorDeContabilidad;
+import Contabilidad.EgresoVariableOtro;
+import Contabilidad.EgresoVariableInsumo;
+import Contabilidad.EgresoFijo;
+import Ventas.Vaso;
+import Ventas.GestorDeVentas;
+import Inventario.Producto;
+import Inventario.GestorDeInventario;
+
+public class MainContabilidad {
+    public static void main(String[] args) {
+        GestorDeVentas gestorVentas = new GestorDeVentas();
+        gestorVentas.registrarCliente("sadad", "675712");
+        Vaso vaso1 = new Vaso("Grande","Leche","perlas","mora","durazno");
+        Vaso vaso2 = new Vaso("Mediano","Agua","perlas","choco","fruti");
+        Vaso vaso3 = new Vaso("Grande","Agua","explosive","macha","ninguno");
+        gestorVentas.agregarVaso(vaso1);
+        gestorVentas.agregarVaso(vaso2);
+        gestorVentas.agregarVaso(vaso3);
+        gestorVentas.confirmarPedido();
+        gestorVentas.terminarDia();
+        gestorVentas.generarReporteVentas();
+        GestorDeInventario gestorInventario = new GestorDeInventario();
+        GestorDeContabilidad gestorContabilidad = new GestorDeContabilidad(gestorInventario,gestorVentas);
+        Producto teverde = new Producto("Té Verde","bolsa","real","6893213");
+        gestorInventario.agregar(teverde);
+        gestorContabilidad.calcularIngreso();
+        // Egresos
+        gestorContabilidad.registrarEgresoFijo(new EgresoFijo("Alquiler", 1200));
+        gestorContabilidad.registrarEgresoInsumo(new EgresoVariableInsumo("Té Verde", 50, 200.0));
+        gestorContabilidad.registrarEgresoOtro(new EgresoVariableOtro("Publicidad", "Facebook Ads", 300));
+        
+        // Imprimir reporte mensual
+        System.out.println(gestorContabilidad.imprimirReporteMensual());
+        
+        // Guardar reporte en el archivo histórico
+        gestorContabilidad.guardarReporteHistorico();
+    }
+}
