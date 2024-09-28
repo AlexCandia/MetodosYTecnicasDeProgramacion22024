51868b737507bdd03b7a630e39cc7a761e9fe3d4
edwcand <167575932+AlexCandia@users.noreply.github.com>
2024-09-27 20:46:00 -0400

    Main Contabilidad con vinculacion a inventario

--- MetodosYTecnicasDeProgramacion22024/MetodosYTecnicasDeProgramacion22024/src/main/java/MainContabilidad.java
+++ MetodosYTecnicasDeProgramacion22024/MetodosYTecnicasDeProgramacion22024/src/main/java/MainContabilidad.java
@@ -1,19 +1,30 @@
 public class MainContabilidad {
     public static void main(String[] args) {
-        GestorDeContabilidad gestor = new GestorDeContabilidad();
-        
-        // Simulamos ingresos y egresos
-        gestor.registrarIngreso(5000);
-        
+        GestorDeVentas gestorVentas = new GestorDeVentas();
+        gestorVentas.registrarCliente("sadad", "675712");
+        Vaso vaso1 = new Vaso("Grande","Leche","perlas","mora","durazno");
+        Vaso vaso2 = new Vaso("Mediano","Agua","perlas","choco","fruti");
+        Vaso vaso3 = new Vaso("Grande","Agua","explosive","macha","ninguno");
+        gestorVentas.agregarVaso(vaso1);
+        gestorVentas.agregarVaso(vaso2);
+        gestorVentas.agregarVaso(vaso3);
+        gestorVentas.confirmarPedido();
+        gestorVentas.terminarDia();
+        gestorVentas.generarReporteVentas();
+        GestorDeInventario gestorInventario = new GestorDeInventario();
+        GestorDeContabilidad gestorContabilidad = new GestorDeContabilidad(gestorInventario,gestorVentas);
+        Producto teverde = new Producto("Té Verde","bolsa","real","6893213");
+        gestorInventario.agregar(teverde);
+        gestorContabilidad.calcularIngreso();
         // Egresos
-        gestor.registrarEgresoFijo(new EgresoFijo("Alquiler", 1200));
-        gestor.registrarEgresoInsumo(new EgresoVariableInsumo("Té Verde", 50, 200));
-        gestor.registrarEgresoOtro(new EgresoVariableOtro("Publicidad", "Facebook Ads", 300));
+        gestorContabilidad.registrarEgresoFijo(new EgresoFijo("Alquiler", 1200));
+        gestorContabilidad.registrarEgresoInsumo(new EgresoVariableInsumo("Té Verde", 50, 200.0));
+        gestorContabilidad.registrarEgresoOtro(new EgresoVariableOtro("Publicidad", "Facebook Ads", 300));
         
         // Imprimir reporte mensual
-        System.out.println(gestor.imprimirReporteMensual());
+        System.out.println(gestorContabilidad.imprimirReporteMensual());
         
         // Guardar reporte en el archivo histórico
-        gestor.guardarReporteHistorico();
+        gestorContabilidad.guardarReporteHistorico();
     }
 }
