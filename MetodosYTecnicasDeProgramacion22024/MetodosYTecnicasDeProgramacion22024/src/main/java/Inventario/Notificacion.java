c0c071ade33af9d40433cd51cea75ccf96f27b22
adrianamfz <202300665@est.umss.edu>
2024-10-16 16:32:42 -0400

    Notificacion.java

--- MetodosYTecnicasDeProgramacion22024/MetodosYTecnicasDeProgramacion22024/src/main/java/Inventario/Notificacion.java
+++ MetodosYTecnicasDeProgramacion22024/MetodosYTecnicasDeProgramacion22024/src/main/java/Inventario/Notificacion.java
@@ -1,7 +1,8 @@
 package Inventario;
 
-
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -23,5 +24,33 @@ public class Notificacion {
         this.mensaje=mensaje;
         this.origen=origen;
         fecha= new Date();
+        notificaciones.add(this); // Agregamos la notificación a la lista
+    }
+    public static void guardarNotificacion(Notificacion notificacion) {
+        notificaciones.add(notificacion);
+    }
+    public static void eliminarNotificacion(Notificacion notificacion) {
+        notificaciones.remove(notificacion);
+    }
+    public static void mostrarNotificacionesPorOrigen(String origen) {
+        for (Notificacion notificacion : notificaciones) {
+            if (notificacion.origen.equals(origen)) {
+                System.out.println("Motivo: " + notificacion.motivo);
+                System.out.println("Mensaje: " + notificacion.mensaje);
+                System.out.println("Fecha: " + notificacion.fecha);
+                System.out.println("Origen: " + notificacion.origen);
+                System.out.println("------------------------");
+            }
+        }
+    }
+    public static void mostrarTodasLasNotificaciones() {
+        for (Notificacion notificacion : notificaciones) {
+            System.out.println("Motivo: " + notificacion.motivo);
+            System.out.println("Mensaje: " + notificacion.mensaje);
+            System.out.println("Fecha: " + notificacion.fecha);
+            System.out.println("Origen: " + notificacion.origen);
+            System.out.println("------------------------");
+        }
     }
 }
+
