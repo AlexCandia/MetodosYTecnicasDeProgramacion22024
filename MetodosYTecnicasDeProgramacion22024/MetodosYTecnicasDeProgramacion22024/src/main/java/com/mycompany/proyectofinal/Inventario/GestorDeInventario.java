0bb84e099eac3c84ba5e8d1dcae6dc2ce7c84ca1
NeosV6 <velahinojosajosealfredo@gmail.com>
2024-11-14 21:19:08 -0400

    agregue spacios

--- MetodosYTecnicasDeProgramacion22024/MetodosYTecnicasDeProgramacion22024/src/main/java/com/mycompany/proyectofinal/Inventario/GestorDeInventario.java
+++ MetodosYTecnicasDeProgramacion22024/MetodosYTecnicasDeProgramacion22024/src/main/java/com/mycompany/proyectofinal/Inventario/GestorDeInventario.java
@@ -36,6 +36,9 @@ public class GestorDeInventario {
                 existe = true;
                break;
             }
+            
+            
+            
         }
      if (!existe){
             productos.add(producto);
